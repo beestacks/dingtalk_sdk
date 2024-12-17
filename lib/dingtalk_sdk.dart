d49e814721e105b81371f1ee399564821c9c6245
Amoyensis <amoyensis@outlook.com>
2024-12-17 11:07:18 +0800

    add export

--- lib/dingtalk_sdk.dart
+++ lib/dingtalk_sdk.dart
@@ -3,15 +3,31 @@ import 'package:dingtalk_sdk/response.dart';
 import 'dingtalk_sdk_method_channel.dart';
 import 'dingtalk_sdk_platform_interface.dart';
 
-class DingtalkSdk {
-  static Stream<BaseResponse> get dingtalkResponseEventHandler => MethodChannelDingtalkSdk.dingtalkResponseEventHandler;
+export 'package:dingtalk_sdk/dingtalk_sdk.dart';
 
-  static Future<bool?> registerAppWithIdentifierForOrgApp({required String appId, required String bundleId}) {
-    return DingtalkSdkPlatform.instance.registerAppWithIdentifierForOrgApp(appId, bundleId);
+class DingtalkSdk {
+  static Stream<BaseResponse> get dingtalkResponseEventHandler =>
+      MethodChannelDingtalkSdk.dingtalkResponseEventHandler;
+
+  static Future<bool?> registerAppWithIdentifierForOrgApp(
+      {required String appId, required String bundleId}) {
+    return DingtalkSdkPlatform.instance
+        .registerAppWithIdentifierForOrgApp(appId, bundleId);
   }
 
-  static Future<bool?> sendAuthForOrgApp({required String redirectUrl, String responseType = 'code', String scope = 'openid', String prompt = 'consent', String state = '', String nonce = ''}) {
-    return DingtalkSdkPlatform.instance.sendAuthForOrgApp(redirectUrl, responseType: responseType, scope: scope, prompt: prompt, state: state, nonce: nonce);
+  static Future<bool?> sendAuthForOrgApp(
+      {required String redirectUrl,
+      String responseType = 'code',
+      String scope = 'openid',
+      String prompt = 'consent',
+      String state = '',
+      String nonce = ''}) {
+    return DingtalkSdkPlatform.instance.sendAuthForOrgApp(redirectUrl,
+        responseType: responseType,
+        scope: scope,
+        prompt: prompt,
+        state: state,
+        nonce: nonce);
   }
 
   static Future<bool?> isDingtalkInstalled() {
